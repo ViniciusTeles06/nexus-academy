d7637a937916a3e3e3c35c5e7f01d6fccd36087e
Vinicius Teles <vinicciusteles13@gmail.com>
2026-08-31 20:55:44 -0300

    chore: configure PostgreSQL database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+
+import environ
+
+
 """
 Django settings for config project.
 
@@ -15,12 +20,18 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    DJANGO_DEBUG=(bool, False),
+)
+
+environ.Env.read_env(BASE_DIR / ".env")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5lgn7u=400%rd5psmix5ycfu5%@kj^7-18fo51cnd6g5sm@$z+'
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
