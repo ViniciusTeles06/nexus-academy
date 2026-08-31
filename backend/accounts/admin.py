43ec652794e01be74d4bfc53f2bc6b424075d0bd
Vinicius Teles <vinicciusteles13@gmail.com>
2026-08-31 11:24:03 -0300

    feat: add custom user model

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,113 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "role",
+        "is_active",
+        "is_staff",
+        "created_at",
+    )
+
+    list_filter = (
+        "role",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "is_email_verified",
+    )
+
+    search_fields = (
+        "email",
+        "first_name",
+        "last_name",
+    )
+
+    ordering = ("email",)
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "password",
+                )
+            },
+        ),
+        (
+            "Informações pessoais",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "avatar",
+                )
+            },
+        ),
+        (
+            "Perfil acadêmico",
+            {
+                "fields": (
+                    "role",
+                    "is_email_verified",
+                )
+            },
+        ),
+        (
+            "Permissões",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Datas importantes",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                    "created_at",
+                    "updated_at",
+                )
+            },
+        ),
+    )
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "role",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                ),
+            },
+        ),
+    )
